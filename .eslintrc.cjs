ac649127efc17b5a8ccb30e13deac593813048b0
Vyacheslav Zavyalov <slavyan1507@mail.ru>
2024-01-11 14:33:10 +0300

    chore/configured eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,46 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:import/typescript",
+    "prettier",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh", "jsx-a11y", "prettier", "import", "simple-import-sort"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
   },
-}
+  overrides: [
+		// override "simple-import-sort" config
+		{
+			files: ["*.js", "*.jsx", "*.ts", "*.tsx"],
+			rules: {
+				"simple-import-sort/imports": [
+					"error",
+					{
+						groups: [
+							// `react` first, then packages starting with a character and @
+							["^react$", "^[a-z]", "^@[a-z]"],
+							// Aliases starting with `@`
+							["^@"],
+							// Packages starting with `~`
+							["^~"],
+							// Imports starting with `../`
+							["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+							// Imports starting with `./`
+							["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+							// Style imports
+							["^.+\\.s?css$"],
+							// Side effect imports
+							["^\\u0000"],
+						],
+					},
+				],
+			},
+		},
+	],
+};
