7ecf57b54c36bb3eb44733b101db161cc7683e14
Vyacheslav Zavyalov <slavyan1507@mail.ru>
2024-01-24 13:52:36 +0300

    chore/Added setting for babelrc

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,15 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({ babel: { babelrc: true } })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  css: {
+    modules: {
+      localsConvention: "camelCase",
     },
   },
 });
